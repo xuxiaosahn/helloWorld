98ec1e71df1eee010a06a564ac1bcad2eb516ec1
小山 <@xuxiaoshan>
2014-05-06 16:39:56 +0800

    maven测试工程

---
+++ HelloWorld/src/main/java/foo/App.java
@@ -0,0 +1,16 @@
+package foo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String sayHello(){
+		return "Hello World";
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( new App().sayHello() );
+    }
+}
